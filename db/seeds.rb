ea0be56b5b78c62ef9f84df6e8e775efebd790f1
Paul Schauss <p+github@schauss.fr>
2022-02-17 09:11:37 +0100

    fin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Flat.destroy_all
+
+puts 'Creating 20 fake flats...'
+20.times do
+  flat = Flat.new(
+    name: Faker::Movie.title,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Hipster.paragraph,
+    banner_url: 'https://source.unsplash.com/random',
+    price_per_night: rand(40..250),
+    number_of_guests: rand(2..8)
+  )
+  flat.save!
+end
+puts 'Finished!'
